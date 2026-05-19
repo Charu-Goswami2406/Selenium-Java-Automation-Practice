973706e02e6be8ba64527f5739f78b8077d7ab80
Charu Goswami <workmailforcharu@gmail.com>
2026-05-19 20:05:21 +0530

    Capturing multiple Elements from webpage.

--- selenium/src/test/java/seleniumwebdriverlaunch/seleniumlocators.java
+++ selenium/src/test/java/seleniumwebdriverlaunch/seleniumlocators.java
@@ -1,8 +1,12 @@
 package seleniumwebdriverlaunch;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class seleniumlocators {
 	public static void main(String[]args) throws InterruptedException{
@@ -20,8 +24,21 @@ public class seleniumlocators {
 		System.out.println(textid);
 		
 		//linktext & partiallinktext
+		//driver.findElement(By.linkText("Return to index")).click();
+		//driver.findElement(By.partialLinkText("index")).click();
 		
 		
+		List<WebElement> l = driver.findElements(By.className("form-control"));
+		System.out.println(l.size());
+//		for(WebElement we:l) {
+//			System.out.println(we.getTagName());
+//		}
+		
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		System.out.println("There is" + links.size() + " link on this webpage");
+		
+		List<WebElement> images = driver.findElements(By.tagName("img"));
+		System.out.println("There are " + images.size() + " images on this webpage");;
 		driver.quit();
 		
 	}
